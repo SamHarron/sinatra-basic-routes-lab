89ac27faab145e5a5092b702d6b822bbbb8f934b
SamHarron <harronsam@gmail.com>
2021-02-09 15:05:14 -0500

    commit tree 6cb34de5632582c46f0f13884c120ba9a48497d1

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Things about me.
+        Try putting the following at the end of the URL at the top:
+        /name
+        /hometown
+        /favorite-song"
+    end
+
+    get '/name' do
+        "My name is Samuel Harron"
+    end
+
+    get '/hometown' do
+        "My hometown is Roanoke, Virginia"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is John Deer Green"
+    end
+
 end
